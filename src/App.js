da6b15c8d7e7254dfef5d47def8f32c9f8418257
Andi Faizal <andifaizal647@gmail.com>
2022-01-11 23:08:25 +0700

    makestyle css in app.css and app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <h1 className="header">Todo List</h1>
+        <ul>
+          <li>
+            <a href="'/">Hallo</a>
+          </li>
+        </ul>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
